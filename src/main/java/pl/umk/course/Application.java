3c4b8a92ad00420d72060460fb013befb0edc0a1
Miroslaw Golda <miroslaw.golda@gmail.com>
2014-10-25 21:15:39 +0200

    step 3 - application changed to a spring one

--- src/main/java/pl/umk/course/Application.java
+++ src/main/java/pl/umk/course/Application.java
@@ -1,34 +1,14 @@
 package pl.umk.course;
 
-import pl.umk.course.exceptions.EncodingException;
-import pl.umk.course.utils.AlphabetValidator;
-import pl.umk.course.utils.Encoder;
-import pl.umk.course.utils.Rot13Encoder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
+@EnableAutoConfiguration
+@ComponentScan
 public class Application {
 
-    public final static void main(String [] args) {
-        Application application = new Application(new Rot13Encoder(new AlphabetValidator()));
-
-        if (args.length != 1) {
-            System.out.println("Error: Give one arg with text");
-            return;
-        }
-
-        application.run(args[0]);
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
-
-    public void run(String arg) {
-        try {
-            System.out.println(encoder.encrypt(arg));
-        } catch (EncodingException encodingException) {
-            System.out.println("Error: " + encodingException.getMessage());
-        }
-    }
-
-    public Application(Encoder encoder) {
-        this.encoder = encoder;
-    }
-
-    private final Encoder encoder;
 }
