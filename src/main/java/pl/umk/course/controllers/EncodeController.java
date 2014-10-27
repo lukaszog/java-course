95613230b250825ffc180086e65be4a485e5d1b5
Miroslaw Golda <miroslaw.golda@gmail.com>
2014-10-27 21:57:22 +0100

    step 7 - text as a path variable

--- src/main/java/pl/umk/course/controllers/EncodeController.java
+++ src/main/java/pl/umk/course/controllers/EncodeController.java
@@ -1,10 +1,7 @@
 package pl.umk.course.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.umk.course.exceptions.EncodingException;
 import pl.umk.course.utils.Encoder;
 
@@ -18,8 +15,8 @@ public class EncodeController {
         return "Hello Spring!";
     }
 
-    @RequestMapping(value = "/encode", method = RequestMethod.GET)
-    public String encode(@RequestParam String text) throws EncodingException {
+    @RequestMapping(value = "/encode/{text}", method = RequestMethod.GET)
+    public String encode(@PathVariable String text) throws EncodingException {
         return encoder.encrypt(text);
     }
 
